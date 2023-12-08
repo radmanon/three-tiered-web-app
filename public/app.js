3a20f4fc7b124dce7d4b48a67c102f234c79f030
Radman Mohammadi <radmanon01@gmail.com>
2023-12-07 20:40:01 -0800

    the app.js - index.js - index.html - styles.css has updated regarding the design and database query and the html changed reagrding the structure of table

--- public/app.js
+++ public/app.js
@@ -1,45 +1,63 @@
-// Function to get user timeline based on the input user ID
-function getUserTimeline() {
-  // Get the user ID from the input field
-  const userIdInput = $('#userIdInput').val();
+// AJAX call to retrieve user timeline data
+const userId = '1'; // Replace with the actual user ID
+$.ajax({
+  url: `http://localhost:8000/timeline/${userId}`,
+  method: 'GET',
+  success: function (data) {
+    // Handle the retrieved data and update the UI
+    displayUserTimeline(data);
+  },
+  error: function (error) {
+    console.error('Error retrieving user timeline:', error);
+  },
+});
 
-  // Check if the user ID is not empty
-  if (userIdInput.trim() === '') {
-    alert('Please enter a valid User ID');
-    return;
-  }
-
-  // AJAX call to retrieve user timeline data
-  $.ajax({
-    url: `http://localhost:8000/timeline/${userIdInput}`,
-    method: 'GET',
-    success: function (data) {
-      // Handle the retrieved data and update the UI
-      displayUserTimeline(data);
-    },
-    error: function (error) {
-      console.error('Error retrieving user timeline:', error);
-    },
-  });
-}
-
-// Function to display user timeline data on the page
+// Function to display user timeline data on the page in a single table
 function displayUserTimeline(data) {
   const userTimelineDiv = $('#userTimeline');
 
   // Clear existing content
   userTimelineDiv.empty();
 
-  // Iterate through the retrieved data and append to the page
+  // Create a table element
+  const table = $('<table>').addClass('user-timeline-table');
+
+  // Create table header
+  const tableHeader = $('<thead>').append(
+    $('<tr>').append(
+      $('<th>').text('User ID'),
+      $('<th>').text('User'),
+      $('<th>').text('Date'),
+      $('<th>').text('Text'),
+      $('<th>').text('Time'),
+      $('<th>').text('Views')
+    )
+  );
+
+  // Append the header to the table
+  table.append(tableHeader);
+
+  // Create table body
+  const tableBody = $('<tbody>');
+
+  // Iterate through the retrieved data and append rows to the table body
   data.forEach(post => {
-    const postDiv = $('<div>');
-    postDiv.append(`<p><strong>User:</strong> ${post.user_name}</p>`);
-    postDiv.append(`<p><strong>Date:</strong> ${post.date_of_post}</p>`);
-    postDiv.append(`<p><strong>Text:</strong> ${post.text_post}</p>`);
-    postDiv.append(`<p><strong>Time:</strong> ${post.time_of_post}</p>`);
-    postDiv.append(`<p><strong>Views:</strong> ${post.views}</p>`);
-    postDiv.append('<hr>');
-    
-    userTimelineDiv.append(postDiv);
+    const row = $('<tr>').append(
+      $('<td>').text(post.user_id),
+      $('<td>').text(post.user_name),
+      $('<td>').text(post.date_of_post),
+      $('<td>').text(post.text_post),
+      $('<td>').text(post.time_of_post),
+      $('<td>').text(post.views)
+    );
+
+    // Append the row to the table body
+    tableBody.append(row);
   });
+
+  // Append the table body to the table
+  table.append(tableBody);
+
+  // Append the table to the userTimelineDiv
+  userTimelineDiv.append(table);
 }
